8e02dc01b855b043db00815a943e1e5b06794bce
ntsatsura15 <n.tsatsura15@gmail.com>
2024-09-15 10:33:40 +0200

    Changed for hw-2.1.4

--- task_score.js
+++ task_score.js
@@ -1,13 +1,14 @@
 //Расчет среднего балла за домашние задания студента по модулям
 let nameStudent = "Nastya";
 let surnameStudent = "Tsatsura";
+let shortNameStudent = nameStudent.slice(0, 1);
 
 const introScore = 100;
 const gitScore = 319;
 const jsScore = 605;
 
-let averageMark = (introScore + gitScore + jsScore) / 3;
+let averageMark = Math.ceil((introScore + gitScore + jsScore) / 3);
 
 console.log(
-  `The ${nameStudent} ${surnameStudent} average mark is ${averageMark}`
+  `The ${shortNameStudent}. ${surnameStudent} average mark is ${averageMark}`
 );
